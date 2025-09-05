7fe18790cf7cdcf3247b3beec92fd63fa384fbec
lucasbeneti <lbeneti@gmail.com>
2025-09-04 22:16:36 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import * as THREE from 'three';
+import { Renderer } from './Renderer';
+import { Camera } from './Camera';
+import { player } from './Player';
+
+import './style.css';
+
+const scene = new THREE.Scene();
+scene.add(player);
+
+const ambienteLight = new THREE.AmbientLight();
+scene.add(ambienteLight);
+
+const dirLight = new THREE.DirectionalLight();
+dirLight.position.set(-100, -100, 200);
+scene.add(dirLight);
+
+const camera = Camera();
+player.add(camera);
+
+const renderer = Renderer();
+renderer.render(scene, camera);
